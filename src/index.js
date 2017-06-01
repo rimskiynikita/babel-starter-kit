d5e420b34f9a5f8e6594b84c12179a422321f35f
Никита Римский <Vireshetov152@gmail.com>
2017-06-01 17:51:38 +0300

    added mongo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,52 @@
-/**
- * Babel Starter Kit (https://www.kriasoft.com/babel-starter-kit)
- *
- * Copyright © 2015-2016 Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import Promise from 'bluebird'
+import bodyParser from 'body-parser'
+import saveDataInDb from './saveDataInDb'
+import Pet from './models/Pet'
+import User from './models/User'
 
-export Greeting from './Greeting.js';
+mongoose.Promise = Promise
+mongoose.connect('mongodb://rupor.space/ruporDB');
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/clear', async (req, res) => {
+	await User.remove({})
+	await Pet.remove({})
+	return res.send('OK')
+})
+
+app.get('/users', async (req, res) => {
+	const users = await User.find()
+	return res.json(users)
+})
+
+app.get('/pets', async (req, res) => {
+	const pets = await Pet.find().populate('owner')
+	return res.json(pets)
+})
+
+app.post('/data', async (req, res) => {
+	const data = req.body
+	if (!data.user) return res.status(400).send('user required')
+	if (!data.pets) data.pets = []
+
+	const user = await User.findOne({
+		name: data.user.name
+	})
+	if (user) return res.status(400).send('user.name exists')
+	try {
+		const result = await saveDataInDb(data)
+		return res.json(result)
+	} catch(err) {
+		return res.status(500).json(err)
+	}
+})
+
+app.listen(3000, 'localhost', () => {
+	console.log('You are listening to port 3000!')
+})
