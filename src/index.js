e7d6ce5164a0677ffe11be5a97488ecd0e1803d3
rimskiynikita <rimskiy.nikita@gmail.com>
2017-06-09 01:03:33 +0300

    meetings offset

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.get('/users', async (req, res) => {
 	return res.json(users)
 })
 
-app.get('/meetings', async (req, res) => {
+app.get('/meetings?:offset?:count', async (req, res) => {
   const offset = await parseInt(req.params.offset)
   const count = await parseInt(req.params.count)
 	const meetings = await Meeting.find().skip(offset).limit(count)
